20039e87ee9bba80b33f56f671bf876c117ffcc1
ASRSQ <a.s.rwar@gmail.com>
2023-04-21 16:27:47 -0300

    user-prefeitura

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\FgtsController;
 use App\Http\Controllers\PrefeituraController;
 use App\Http\Controllers\DadosController;
 use App\Http\Controllers\UserDataController;
+use App\Http\Controllers\UserPrefeituraController;
 
 /*
 |--------------------------------------------------------------------------
