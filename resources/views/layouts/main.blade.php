0cdf95b24b11116ef36da6cbc80f1afb1c7f0c6e
ASRSQ <a.s.rwar@gmail.com>
2023-04-20 22:08:34 -0300

    botao menu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -77,15 +77,15 @@
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   <script>
-    document.addEventListener('DOMContentLoaded', function () {
+      document.addEventListener('DOMContentLoaded', function () {
       const navbarToggler = document.querySelector('.navbar-toggler');
-      const sidebarMenu = document.querySelector('#sidebarMenu');
-  
+      const sidebarMenu = new bootstrap.Offcanvas(document.querySelector('#sidebarMenu'));
+
       navbarToggler.addEventListener('click', function () {
-        sidebarMenu.classList.toggle('d-none');
-        sidebarMenu.classList.toggle('d-sm-block');
+        sidebarMenu.toggle();
       });
     });
+
   </script>
   
 </body>
