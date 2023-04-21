712f49ceb5879b950f497c126b7965a7b81272ac
ASRSQ <a.s.rwar@gmail.com>
2023-04-21 16:28:37 -0300

    user-prefeitura

--- resources/views/user-prefeitura.blade.php
+++ resources/views/user-prefeitura.blade.php
@@ -13,95 +13,171 @@
     <div style="display: flex; flex-wrap: wrap;">
       <div style="flex: 1 1 50%;">
         <div>
-          <strong>Nome:</strong>
-          <span>{{ $fgt->nome }}</span>
+          <strong>NOME_A:</strong>
+          <span>{{ $prefeitura->NOME_A }}</span>
+        </div>
+        <div>
+          <strong>NASC:</strong>
+          <span>{{ date("d/m/Y", strtotime($prefeitura->NASC)) }}</span>
         </div>
         <div>
           <strong>CPF:</strong>
-          <span>{{ $fgt->CPF2 }}</span>
+          <span>{{ $prefeitura->CPF }}</span>
         </div>
         <div>
-          <strong>Nascimento:</strong>
-          <span>{{ date('d/m/Y', strtotime($fgt->nasc)) }}</span>
+          <strong>LOGR:</strong>
+          <span>{{ $prefeitura->LOGR }}</span>
         </div>
         <div>
-          <strong>Endereço:</strong>
-          <span>{{ $fgt->endereco }}</span>
+          <strong>NUM:</strong>
+          <span>{{ $prefeitura->NUM }}</span>
         </div>
         <div>
-          <strong>Bairro:</strong>
-          <span>{{ $fgt->bairro }}</span>
+          <strong>COMPL:</strong>
+          <span>{{ $prefeitura->COMPL }}</span>
         </div>
         <div>
-          <strong>Cidade:</strong>
-          <span>{{ $fgt->cidade }}</span>
+          <strong>BAIRRO:</strong>
+          <span>{{ $prefeitura->BAIRRO }}</span>
         </div>
         <div>
           <strong>CEP:</strong>
-          <span>{{ $fgt->cep }}</span>
+          <span>{{ $prefeitura->CEP }}</span>
         </div>
         <div>
-          <strong>Móvel 1:</strong>
-          <span>{{ $fgt->movel1 }}</span>
+          <strong>CIDADE:</strong>
+          <span>{{ $prefeitura->CIDADE }}</span>
         </div>
         <div>
-          <strong>Móvel 2:</strong>
-          <span>{{ $fgt->movel2 }}</span>
+          <strong>UF:</strong>
+          <span>{{ $prefeitura->UF }}</span>
         </div>
         <div>
-          <strong>Móvel 3:</strong>
-          <span>{{ $fgt->movel3 }}</span>
+          <strong>DDD:</strong>
+          <span>{{ $prefeitura->DDD }}</span>
+        </div>
+        <div>
+          <strong>TEL:</strong>
+          <span>{{ $prefeitura->TEL }}</span>
+        </div>
+        <div>
+          <strong>DDD2:</strong>
+          <span>{{ $prefeitura->DDD2 }}</span>
+        </div>
+        <div>
+          <strong>TEL2:</strong>
+          <span>{{ $prefeitura->TEL2 }}</span>
+        </div>
+        <div>
+          <strong>DDDCEL:</strong>
+          <span>{{ $prefeitura->DDDCEL }}</span>
+        </div>
+        <div>
+          <strong>CEL:</strong>
+          <span>{{ $prefeitura->CEL }}</span>
+        </div>
+        <div>
+          <strong>CELOP:</strong>
+          <span>{{ $prefeitura->CELOP }}</span>
         </div>
       </div>
       <div style="flex: 1 1 50%;">
         <div>
-          <strong>ID:</strong>
-          <span>{{ $fgt->id }}</span>
+          <strong>TIPO:</strong>
+          <span>{{ $prefeitura->TIPO }}</span>
         </div>
         <div>
-          <strong>CBO:</strong>
-          <span>{{ $fgt->cbo }}</span>
+          <strong>TP:</strong>
+          <span>{{ $prefeitura->TP }}</span>
         </div>
-      
         <div>
-          <strong>CNAE:</strong>
-          <span>{{ $fgt->cnae }}</span>
+          <strong>EMAIL:</strong>
+          <span>{{ $prefeitura->EMAIL }}</span>
         </div>
         <div>
           <strong>CNPJ:</strong>
-          <span>{{ $fgt->cnpj }}</span>
+          <span>{{ $prefeitura->CNPJ }}</span>
         </div>
         <div>
-          <strong>Complemento:</strong>
-          <span>{{ $fgt->complemento }}</span>
+          <strong>SEXO:</strong>
+          <span>{{ $prefeitura->SEXO }}</span>
         </div>
-        <div>
-          <strong>Email 1:</strong>
-          <span>{{ $fgt->email1 }}</span>
-        </div>
-        <div>
-          <strong>Fixo 1:</strong>
-          <span>{{ $fgt->fixo1 }}</span>
-        </div>
-        <div>
-          <strong>Fixo 2:</strong>
-          <span>{{ $fgt->fixo2 }}</span>
-        </div>
-        <div>
-          <strong>Fixo 3:</strong>
-          <span>{{ $fgt->fixo3 }}</span>
-        </div>
-        <div>
-          <strong>Saldo:</strong>
-          <span>{{ $fgt->saldo }}</
+        
+          <div>
+            <strong>ESCO:</strong>
+            <span>{{ $prefeitura->ESCO }}</span>
           </div>
           <div>
-            <strong>Sexo:</strong>
-            <span>{{ $fgt->sexo }}</span>
+            <strong>NAC:</strong>
+            <span>{{ $prefeitura->NAC }}</span>
           </div>
           <div>
-            <strong>UF:</strong>
-            <span>{{ $fgt->uf }}</span>
+            <strong>CBO:</strong>
+            <span>{{ $prefeitura->CBO }}</span>
+          </div>
+          <div>
+            <strong>ADMISSAO:</strong>
+            <span>{{ $prefeitura->ADMISSAO }}</span>
+          </div>
+          <div>
+            <strong>DESLIG:</strong>
+            <span>{{ $prefeitura->DESLIG }}</span>
+          </div>
+          <div>
+            <strong>SALCONT:</strong>
+            <span>{{ $prefeitura->SALCONT }}</span>
+          </div>
+          <div>
+            <strong>CNAE:</strong>
+            <span>{{ $prefeitura->CNAE }}</span>
+          </div>
+          <div>
+            <strong>CNAE2:</strong>
+            <span>{{ $prefeitura->CNAE2 }}</span>
+          </div>
+          <div>
+            <strong>NJ:</strong>
+            <span>{{ $prefeitura->NJ }}</span>
+          </div>
+          <div>
+            <strong>SALDEZ:</strong>
+            <span>{{ $prefeitura->SALDEZ }}</span>
+          </div>
+          <div>
+            <strong>VINC:</strong>
+            <span>{{ $prefeitura->VINC }}</span>
+          </div>
+          <div>
+            <strong>ID:</strong>
+            <span>{{ $prefeitura->ID }}</span>
+          </div>
+          <div>
+            <strong>NOME_B:</strong>
+            <span>{{ $prefeitura->NOME_B }}</span>
+          </div>
+          <div>
+            <strong>CONTATOS_ID:</strong>
+            <span>{{ $prefeitura->CONTATOS_ID }}</span>
+          </div>
+          <div>
+            <strong>ddd3:</strong>
+            <span>{{ $prefeitura->ddd3 }}</span>
+          </div>
+          <div>
+            <strong>telefone3:</strong>
+            <span>{{ $prefeitura->telefone3 }}</span>
+          </div>
+          <div>
+            <strong>ddd4:</strong>
+            <span>{{ $prefeitura->ddd4 }}</span>
+          </div>
+          <div>
+            <strong>telefone4:</strong>
+            <span>{{ $prefeitura->telefone4 }}</span>
+          </div>
+          <div>
+            <strong>TIPO_TELEFONE:</strong>
+            <span>{{ $prefeitura->TIPO_TELEFONE }}</span>
           </div>
         </div>
       </div>
