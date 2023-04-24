f5536a84d7e4e0e094b644da053af997b6060134
ASRSQ <a.s.rwar@gmail.com>
2023-04-24 11:49:37 -0300

    editar cadastro

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,24 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 
 class UserController extends Controller
 {
     public function edit()
-    {
-        $user = User::user();
-        return view('users.edit', compact('user'));
-    }
+        {
+            $user = Auth::user();
+            return view('users.edit', compact('user'));
+        }
 
-    public function update(Request $request)
-    {
-        $user = User::user();
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email,' . $user->id,
-        ]);
+        public function update(Request $request)
+        {
+            $user = Auth::user();
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email,' . $user->id,
+                'phone' => 'required',
+            ]);
 
-        $user->update($request->all());
-        return redirect()->route('home')->with('success', 'Dados atualizados com sucesso');
-    }
+            $user->update($request->all());
+            return redirect()->route('home')->with('success', 'Dados atualizados com sucesso');
+        }
+
 }
