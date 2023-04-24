3a592a34acc07a5510480010fbd2d063e576c102
ASRSQ <a.s.rwar@gmail.com>
2023-04-24 12:59:47 -0300

    editar cadastro

--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -1,33 +1,26 @@
-@extends('layouts.app')
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Editar Dados Cadastrais</div>
+            <div class="panel-body">
+                <form action="{{ route('users.update') }}" method="POST">
+                    @csrf
+                    @method('PUT')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Editar Dados Cadastrais</div>
-                <div class="panel-body">
-                    <form action="{{ route('users.update') }}" method="POST">
-                        @csrf
-                        @method('PUT')
+                    <div class="form-group">
+                        <label for="name">Nome:</label>
+                        <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}" required>
+                    </div>
 
-                        <div class="form-group">
-                            <label for="name">Nome:</label>
-                            <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}" required>
-                        </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" name="email" id="email" class="form-control" value="{{ $user->email }}" required>
+                    </div>
 
-                        <div class="form-group">
-                            <label for="email">Email:</label>
-                            <input type="email" name="email" id="email" class="form-control" value="{{ $user->email }}" required>
-                        </div>
-
-
-                        <button type="submit" class="btn btn-primary">Atualizar</button>
-                        {{-- <a href="{{ route('/') }}" class="btn btn-default">Cancelar</a> --}}
-                    </form>
-                </div>
+                    <button type="submit" class="btn btn-primary mb-3">Atualizar</button>
+                    <a href="{{ route('inicial.index') }}" class="btn btn-default mb-3">Cancelar</a>
+                </form>
             </div>
         </div>
     </div>
 </div>
-@endsection
