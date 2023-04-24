15b68048b63eb3c2cc8998afe4022aa2e7502f3c
ASRSQ <a.s.rwar@gmail.com>
2023-04-24 13:16:54 -0300

    centralizar atualização de cadastro

--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
-<div class="row">
-    <div class="col-md-8 col-md-offset-2">
+<div class="row justify-content-center">
+    <div class="col-md-8 offset-md-2">
         <div class="panel panel-default">
             <div class="panel-heading">Editar Dados Cadastrais</div>
             <div class="panel-body">
