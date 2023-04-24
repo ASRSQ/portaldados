c15726589a33318b08a580ab3ed59ddda0be7aa5
ASRSQ <a.s.rwar@gmail.com>
2023-04-24 11:44:07 -0300

    editar cadastro

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function edit()
+    {
+        $user = Auth::user();
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+        ]);
+
+        $user->update($request->all());
+        return redirect()->route('home')->with('success', 'Dados atualizados com sucesso');
+    }
+}
---
+++ resources/views/auth/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Editar Dados Cadastrais</div>
+                <div class="panel-body">
+                    <form action="{{ route('users.update') }}" method="POST">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="form-group">
+                            <label for="name">Nome:</label>
+                            <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" name="email" id="email" class="form-control" value="{{ $user->email }}" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="phone">Telefone:</label>
+                            <input type="text" name="phone" id="phone" class="form-control" value="{{ $user->phone }}" required>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Atualizar</button>
+                        <a href="{{ route('home') }}" class="btn btn-default">Cancelar</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
